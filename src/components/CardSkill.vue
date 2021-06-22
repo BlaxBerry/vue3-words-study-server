70d23b962142c337999dcef6db0e24c9329d74f8
Chen <chenjiaxu333@gmail.com>
2021-06-23 05:27:26 +0900

    responsive MediaQuery skillcard

---
+++ src/components/CardSkill.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-skill center-align">
+    <img src="../assets/images/javascript.svg" alt="" />
+    <h4>JavaScript</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.card-skill {
+  padding: 1rem 3rem;
+  cursor: pointer;
+  &:hover {
+    background-color: antiquewhite;
+  }
+
+  img {
+    width: 5rem;
+  }
+}
+</style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,22 +3,72 @@
     <!-- title -->
     <h2 class="title">Skills</h2>
     <h3 class="title2">について</h3>
+    <hr />
+    <p class="center-align">Skills</p>
 
     <!-- contnet -->
-    <div>
-      <p class="center-align">Skills</p>
+    <div class="front-end">
+      <h4 class="title3">Front End</h4>
+      <div class="cardskill-container">
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+      </div>
+    </div>
+
+    <div class="server-side">
+       <h4 class="title3">Server Side</h4>
+      <div class="cardskill-container">
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+      </div>
+    </div>
+
+    <div class="data-base">
+       <h4 class="title3">Data Base</h4>
+      <div class="cardskill-container">
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+      </div>
+    </div>
+
+    <div class="others">
+       <h4 class="title3">Others</h4>
+      <div class="cardskill-container">
+        <CardSkill></CardSkill>
+        <CardSkill></CardSkill>
+      </div>
     </div>
 
     <!-- link -->
     <div class="center-align">
       <router-link to="/skills">Show Skills Detail -></router-link>
     </div>
-
   </div>
 </template>
 
 <script>
-export default {};
+import CardSkill from "@/components/CardSkill.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    CardSkill,
+  },
+};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.cardskill-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+</style>
