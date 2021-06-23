af9cc279b02246acc0c8edd393aafba692a9f8f2
Chen <chenjiaxu333@gmail.com>
2021-06-23 15:48:19 +0900

    skills

--- src/components/CardSkill.vue
+++ src/components/CardSkill.vue
@@ -1,19 +1,95 @@
 <template>
-  <div class="cardskill-container">
-    <div
-      class="center-align card-skill"
-      v-for="(item, index) in skills"
-      :key="index"
-    >
-      <img :src="item.svg" alt="" />
-      <h4>{{ item.name }}</h4>
+  <div>
+    <!-- frondEnd -->
+    <div class="front-end">
+      <h4 class="title3">Front End</h4>
+      <div class="cardskill-container">
+        <div class="center-align card-skill">
+          <img src="../assets/images/html5.svg" alt="" />
+          <h4>html5</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/css3.svg" alt="" />
+          <h4>CSS3</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/javascript.svg" alt="" />
+          <h4>JavaScript</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/jquery.svg" alt="" />
+          <h4>jQuery</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/less.svg" alt="" />
+          <h4>Less</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/sass.svg" alt="" />
+          <h4>Sass</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/vue.svg" alt="" />
+          <h4>Vue</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/react.svg" alt="" />
+          <h4>React</h4>
+        </div>
+      </div>
+    </div>
+    <!-- server -->
+    <div class="server-side">
+      <h4 class="title3">Server Side</h4>
+      <div class="cardskill-container">
+        <div class="center-align card-skill">
+          <img src="../assets/images/nodejs.svg" alt="" />
+          <h4>Node.js</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/nuxtjs.svg" alt="" />
+          <h4>Nuxt.js</h4>
+        </div>
+      </div>
+    </div>
+    <!-- data base -->
+    <div class="server-side">
+      <h4 class="title3">Server Side</h4>
+      <div class="cardskill-container">
+        <div class="center-align card-skill">
+          <img src="../assets/images/mongodb.svg" alt="" />
+          <h4>MongoDB</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/postgresql.svg" alt="" />
+          <h4>PostgreSQL</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/mysql.svg" alt="" />
+          <h4>MySQL</h4>
+        </div>
+      </div>
+    </div>
+    <!-- others -->
+    <div class="server-side">
+      <h4 class="title3">Server Side</h4>
+      <div class="cardskill-container">
+        <div class="center-align card-skill">
+          <img src="../assets/images/git.svg" alt="" />
+          <h4>Git</h4>
+        </div>
+        <div class="center-align card-skill">
+          <img src="../assets/images/github.svg" alt="" />
+          <h4>Github</h4>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["skills"],
+  // props: ["skills"],
 };
 </script>
 
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -7,25 +7,23 @@
     <p class="center-align">Skills</p>
 
     <!-- contnet -->
-    <div class="front-end">
+    <CardSkill></CardSkill>
+    <!-- <div class="front-end">
       <h4 class="title3">Front End</h4>
       <CardSkill :skills="frondEnd"></CardSkill>
     </div>
-
     <div class="server-side">
       <h4 class="title3">Server Side</h4>
       <CardSkill :skills="serverSide"></CardSkill>
     </div>
-
     <div class="data-base">
       <h4 class="title3">Data Base</h4>
       <CardSkill :skills="dataBase"></CardSkill>
     </div>
-
     <div class="others">
       <h4 class="title3">Others</h4>
       <CardSkill :skills="otherSkills"></CardSkill>
-    </div>
+    </div> -->
 
     <!-- link -->
     <div class="center-align">
@@ -36,25 +34,25 @@
 
 <script>
 import CardSkill from "@/components/CardSkill.vue";
-// JSON 数据
-import frontEndJson from "@/assets/json/frontEnd.json";
-import serverSideJson from "@/assets/json/server.json";
-import dataBaseJson from "@/assets/json/dataBase.json";
-import otherSkillsJson from "@/assets/json/others.json"
-// 调用
-let frondEnd = frontEndJson;
-let serverSide = serverSideJson;
-let dataBase = dataBaseJson;
-let otherSkills = otherSkillsJson;
+// // JSON 数据
+// import frontEndJson from "@/assets/json/frontEnd.json";
+// import serverSideJson from "@/assets/json/server.json";
+// import dataBaseJson from "@/assets/json/dataBase.json";
+// import otherSkillsJson from "@/assets/json/others.json"
+// // 调用
+// let frondEnd = frontEndJson;
+// let serverSide = serverSideJson;
+// let dataBase = dataBaseJson;
+// let otherSkills = otherSkillsJson;
 
 export default {
   data() {
     return {
       // front-end skills Array
-      frondEnd,
-      serverSide,
-      dataBase,
-      otherSkills
+      // frondEnd,
+      // serverSide,
+      // dataBase,
+      // otherSkills
     };
   },
   components: {
