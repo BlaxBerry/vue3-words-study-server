01b37816127d2752c694a7cc6cc20c25952675ac
Chen <chenjiaxu333@gmail.com>
2021-06-24 16:55:36 +0900

    contact links & footer

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,10 +5,20 @@
       <h1 slot="title1" class="title1">Contact</h1>
       <h2 slot="title2" class="title2">連絡について</h2>
     </Title>
-    
-    <!-- desc -->
-    <p class="center-align">about my skills.......</p>
-    <br />
+
+    <div class="contact-links center-align">
+      <a href="https://twitter.com/chenjiaxu333" target="_blank">
+        <img src="../assets/images/twitter.svg" alt="" />
+      </a>
+
+      <a href="https://github.com/BlaxBerry" target="_blank">
+        <img src="../assets/images/github.svg" alt="" />
+      </a>
+
+      <a href="mailto:chenjiaxu333@gmail.com" target="_blank">
+        <img src="../assets/images/gmail.svg" alt="" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -21,4 +31,21 @@ export default {
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less">
+.contact-links {
+  a {
+    display: inline-block;
+    margin: 1rem;
+    width: 4rem;
+    height: 4rem;
+    img {
+      filter: grayscale(10%);
+      transition: all 0.5s;
+      &:hover {
+        filter: grayscale(0);
+        transform: scale(1.2);
+      }
+    }
+  }
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="footer">
-    Footer
+    <h1>Footerrrrrr</h1>
+    <!-- desc -->
+    <h3 class="title3">
+      最後までご覧頂き、心より感謝致します。
+    </h3>
+    <br />
+    <p>
+      独自の開発しいくうちに、自分の不足も認識しました。
+      今後も必ず深く勉強して改善し、そして専門性を高め、
+      一流のエンジニアを目指して、引き続き頑張っていきたいと思います。
+      <br />
+    </p>
+    <br />
+
+    <div class="version">
+      <a href="https://github.com/BlaxBerry/-Portfolio" target="_blank"
+        >Chen's Portofilo <b>Version 3.0</b>
+      </a>
+    </div>
+    <div class="copyright">
+      <a href="https://github.com/BlaxBerry" target="_blank"
+        >© 2021 BlaxBerry</a
+      >
+    </div>
   </div>
 </template>
 
@@ -8,4 +31,20 @@
 export default {};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.footer {
+  padding: 0 10%;
+  p {
+    font-size: 1.3rem;
+  }
+  a {
+    font-size: 1rem;
+  }
+  .version {
+    text-align: right;
+  }
+  .copyright {
+    text-align: right;
+  }
+}
+</style>
